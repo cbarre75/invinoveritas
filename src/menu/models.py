2b7a4820e0e1603805a6a9780f98f92007ad8522
Clément Barré <cbarre@quantmetry.com>
2023-04-26 13:46:00 +0200

    Ajout date

--- src/menu/models.py
+++ src/menu/models.py
@@ -9,6 +9,7 @@ class Vin(models.Model):
     producteur = models.CharField(max_length=200)
     millesime = models.IntegerField(default=0)
     quantite = models.IntegerField(default=0)
+    date_ajout = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.nom
